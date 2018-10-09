dc2f95e2e5badcbb22f8b78ddc200ac0cd645481
Jamie Land <18668469@students.lincoln.ac.uk>
2018-10-09 11:43:36 +0100

    New programs
    Abs, euler 5 and prime numbers

---
+++ Absolute Value/Absolute Value/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Absolute_Value
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Console.Read();
+            int answer;
+
+            answer = (num < 0) ? -num : num;
+            Console.WriteLine("The absolute value is {0}", answer);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Prime Number/Prime Number/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s_num = Console.ReadLine();
+            int num = Int32.Parse(s_num);
+
+            // Work around so 1 isn't caught later.
+            if (num == 2)
+            {
+                Console.WriteLine("2 is a prime number");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+            for (int i = 2; i < num; i++)
+            {
+                if (num % i == 0)
+                { 
+                    Console.WriteLine("{0} is not a prime number", num);
+                    Console.ReadKey();
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is a prime number", num);
+                    Console.ReadKey();
+                    Environment.Exit(0);
+                }
+            }
+        }
+    }
+}
