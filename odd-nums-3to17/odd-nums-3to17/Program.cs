b383807ad8a14db2807692efefa3801a72d11173
Jamie Land <18668469@students.lincoln.ac.uk>
2018-10-12 11:37:50 +0100

    3 to 17
    I have committed a sin father, and I must repent

---
+++ odd-nums-3to17/odd-nums-3to17/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odd_nums_3to17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 3;
+            while (i >= 3)
+            {
+                if (i == 9)
+                    goto Goodboy;
+
+                if (i == 19)
+                    break;
+                else if (i > 19)
+                {
+                    Console.WriteLine("Missed");
+                    break;
+                }
+
+                Console.WriteLine($"{i}");
+
+            Goodboy:
+                i += 2;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
