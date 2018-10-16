81b91f2582c76d641ca790263a5f0179ad858456
Jamie Land <18668469@students.lincoln.ac.uk>
2018-10-16 11:45:29 +0100

    yes

---
+++ rectangle-maker/rectangle-maker/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rectangle_maker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Character: ");
+            string s_character = Console.ReadLine();
+            char character = Convert.ToChar(s_character);
+            Console.Write("Width: ");
+            string s_width = Console.ReadLine();
+            int width = Convert.ToInt32(s_width);
+            Console.Write("Height: ");
+            string s_height = Console.ReadLine();
+            int height = Convert.ToInt32(s_height);
+            Console.WriteLine();
+
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    Console.Write(character);
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
