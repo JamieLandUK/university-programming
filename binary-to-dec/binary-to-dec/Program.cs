ef8f7ac6a8257e6c59fb84731c04c03c1bb288a7
Jamie Land <18668469@students.lincoln.ac.uk>
2018-10-19 11:45:21 +0100

    this works, i guess?

---
+++ binary-to-dec/binary-to-dec/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace binary_to_dec
+{
+    class Program
+    {
+        static int BinToDec(string input)
+        {
+            // I need to separate the string into the single letters (or numbers)
+            char[] array = input.ToCharArray();
+
+            // Flip the array as binary works as ...32, 16, 8, 4, 2, 1
+            Array.Reverse(array);
+            // Base sum to keep track of all what is inside the for loop.
+            int sum = 0;
+
+            // Going through every number.
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == '1')
+                {
+                    /* If the first number (the units) of the binary is 1,
+                     * It will only need to be given that single 1.
+                     */
+                    if (i == 0)
+                    {
+                        sum += 1;
+                    }
+                    /* However, if it is anything outside of the units column.
+                     * Cast the number to an int so that I can get it to the power
+                     * of the column.
+                     */
+                    else
+                    {
+                        sum += (int)Math.Pow(2, i);
+                    }
+                }
+            }
+
+            // Give it back to the user.
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("What is the binary number you want the decimal of? ");
+            string number = Console.ReadLine();
+            Console.WriteLine($"The decimal of {number} is {BinToDec(number)}.");
+            Console.ReadKey();
+        }
+    }
+}
