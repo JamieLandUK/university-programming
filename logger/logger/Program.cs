9177167b5ddbc806747774d2835fc58762133e6b
Jamie Land <18668469@students.lincoln.ac.uk>
2018-10-26 11:32:30 +0100

    fucc this

---
+++ logger/logger/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace logger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What text would you like to write to the log? ");
+            string text = Console.ReadLine();
+
+            IEnumerable<string> answer = new string[1];
+            answer.Concat(new string[] { $"[{DateTime.Now} -> {text}]" });
+
+            File.AppendAllLines("today.log", answer);
+        }
+    }
+}
+    
