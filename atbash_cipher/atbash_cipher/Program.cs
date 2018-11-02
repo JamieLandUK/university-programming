39eeb9f7bf8bd93103a2e872413ac7ab2fdde813
Jamie Land <18668469@students.lincoln.ac.uk>
2018-11-02 12:08:01 +0000

    yes

---
+++ atbash_cipher/atbash_cipher/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atbash_cipher
+{
+    class Atbash
+    {
+        private char[] alphabet = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+        private char[] atbash_alpha = { 'Z', 'Y', 'X', 'W', 'V', 'U', 'T', 'S', 'R', 'Q', 'P', 'O', 'N', 'M', 'L', 'K', 'J', 'I', 'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A' };
+
+        public string Encode(string plaintext)
+        {
+            string output = "";
+            
+            for (int i = 0; i < plaintext.Length; i++)
+            {
+                output += atbash_alpha[Array.IndexOf(alphabet, plaintext[i])];
+            }
+
+            return output;
+        }
+
+        public string Decode(string ciphertext)
+        {
+            Console.WriteLine();
+            return ciphertext;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Atbash atbash = new Atbash();
+            Console.WriteLine(atbash.Decode("SVOOL GSVIV UIRVMW"));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ messages-oop/messages-oop/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace messages_oop
+{
+    class Message
+    {
+        public string s = "Hello, this is a class!";
+
+        public void DisplayMessage()
+        {
+            Console.WriteLine(s);
+        }
+
+        public void DisplayDate()
+        {
+            Console.WriteLine("Today's date is {0}!", DateTime.Now.ToString("dd/MM/yy"));
+        }
+
+        public void WriteLog(string x)
+        {
+            File.AppendAllText("yes.log", Environment.NewLine + DateTime.Now.ToString("dd/MM/yy: ") + x);
+        }
+
+        public string[] ReadLogFile()
+        {
+            string[] all_messages = File.ReadAllLines("yes.log");
+            foreach (string message in all_messages)
+            {
+                Console.WriteLine(message);
+            }
+            return all_messages;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Message message = new Message();
+            message.DisplayMessage();
+            message.DisplayDate();
+            message.WriteLog(Console.ReadLine());
+            message.ReadLogFile();  
+            Console.ReadKey();
+        }
+    }
+}
